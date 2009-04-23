29e7b2eb61d8d04e462ca504b415d6d14824e923
Sam Merritt <spam@andcheese.org>
2009-04-23 13:44:45 -0700

    world's simplest merb app

--- config/router.rb
+++ config/router.rb
@@ -30,9 +30,6 @@ Merb::Router.prepare do
   # RESTful routes
   # resources :posts
   
-  # Adds the required routes for merb-auth using the password slice
-  slice(:merb_auth_slice_password, :name_prefix => nil, :path_prefix => "")
-
   # This is the default route for /:controller/:action/:id
   # This is fine for most cases.  If you're heavily using resource-based
   # routes, you may want to comment/remove this line to prevent
@@ -41,4 +38,5 @@ Merb::Router.prepare do
   
   # Change this for your home page to be available at /
   # match('/').to(:controller => 'whatever', :action =>'index')
+  match('/').to(:controller => 'hello', :action => 'index')
 end
